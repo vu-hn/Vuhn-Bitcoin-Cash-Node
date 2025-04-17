9770e27bbaab219390170014423a556b91b96723
Andrew #128 <andrew-128-github@protonmail.com>
2025-04-17 21:02:50 +0000

    Resolve CMP0116 deprecation warnings
    The policy in question was introduced in CMake 3.20.
    Full documentation for it is here https://cmake.org/cmake/help/v4.0/policy/CMP0116.html.
    
    See MR !1946 for full description.

--- cmake/modules/CustomCommandWithDepFile.cmake
+++ cmake/modules/CustomCommandWithDepFile.cmake
@@ -1,9 +1,14 @@
+#!/usr/bin/env python3
+# Copyright (c) 2020-2025 The Bitcoin developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 # Allow to easily create a custom command that uses dep file.
 # depfile are a ninja only feature and a hard fail using other generators.
 
-# define how DEPFILEs are treated
-if(${CMAKE_VERSION} VERSION_GREATER "3.19")
-  cmake_policy(SET CMP0116 OLD)
+# Define how DEPFILEs are treated (Policy CMP0116, introduced in CMake 3.20)
+if(POLICY CMP0116)
+	cmake_policy(SET CMP0116 NEW)
 endif()
 
 function(add_custom_command_with_depfile)
