e9f25d7596f1160ff4119977333f987da97dc74c
Calin Culianu <calin.culianu@gmail.com>
2025-12-21 23:02:25 +0200

    Remove boost dependency: `<boost/range/iterator.hpp>`
    The boost range iterator adaptor was extraneous and un-needed. It was
    only used in rwcollection.h. One can do what rwcollection.h required just
    with C++ types, which this commit does.

--- test/lint/lint-includes.sh
+++ test/lint/lint-includes.sh
@@ -60,7 +60,6 @@ EXPECTED_BOOST_INCLUDES=(
     boost/multi_index/ordered_index.hpp
     boost/multi_index/sequenced_index.hpp
     boost/multi_index_container.hpp
-    boost/range/iterator.hpp
     boost/signals2/connection.hpp
     boost/signals2/last_value.hpp
     boost/signals2/signal.hpp
